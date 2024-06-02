0aaa2962859f0219aa81f3359ee8c06b0339a20e
Shagun917 <Shagun007@gmail.com>
2024-06-02 22:42:16 +0530

    implemented dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ const App = () => {
     setShowLoading(true);
     setTimeout(() => {
       setShowLoading(false);
-      setShowSecondComponent(true);
     }, 2000);
   };
   const handleColorChange = () => {
